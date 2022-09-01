57b84b146b7417c9a82803a34cc010818b7d4875
Taha <taha@bluelogic.ae>
2022-09-01 14:53:17 +0400

    websocket conditions set - logic pending, place order api params created - signature and response handling pending

--- helpers.py
+++ helpers.py
@@ -1,11 +1,39 @@
-def place_bid_order():
+import config
+import requests
+import json
+import time
+
+API_KEY = config.API_KEY
+API_SECRET = config.API_SECRET
+
+API_URL = "https://testnet.binance.vision/api/v3/"
+auth = requests.HTTPBasicAuth(API_KEY, API_SECRET)
+
+def place_bid_order(symbol, price):
+    URL = API_URL + "order"
+    DATA = {
+        "symbol": symbol,
+        "type": "STOP_LOSS",
+        "side": "BUY",
+        "stopPrice": price,
+        "quantity": 1,
+        "timestamp": round(time.time() * 1000),
+        "signature": "" # HMAC SHA256 of secret key and total params
+    }
+    
+    response = requests.post(url = URL, data = DATA, auth=auth)
+    
+    return response
+    
+def place_ask_order(symbol, price):
     pass
 
-def place_ask_order():
+def cancel_bid_order(symbol, orderId):
     pass
 
-def cancel_bid_order():
+def cancel_ask_order(symbol, orderId):
     pass
 
-def cancel_ask_order():
+def cancel_all_orders(symbol):
+    # test if cancel all orders api can be used to cancel both bid and ask orders
     pass
--- liquidity_engine.py
+++ liquidity_engine.py
@@ -1,7 +1,14 @@
-import websocket, json, config
+from hmac import compare_digest
+import websocket, json
+from helpers import place_ask_order, place_bid_order, cancel_ask_order, cancel_bid_order
 
-TRADING_PAIR = "btcusdt"
-TIME_INTERVAL = "1m"
+TRADING_PAIR = "BTCUSDT"
+TIME_INTERVAL = "1s" 
+COMPARE_PRICE = 0.0
+CURRENT_PRICE = 0.0
+ASK_ORDER_PRICE = 0.0
+BID_ORDER_PRICE = 0.0
+FIRST_PRICE_SET = False
 
 SOCKET = f'wss://stream.binance.com:9443/ws/'+TRADING_PAIR+'@kline_'+TIME_INTERVAL
 
@@ -12,7 +19,31 @@ def on_close(ws):
     print("***Closed Connection***")
     
 def on_message(ws, message):
-    pass
+    global COMPARE_PRICE, CURRENT_PRICE, ASK_ORDER_PRICE, BID_ORDER_PRICE, FIRST_PRICE_SET
+    
+    price_data = json.loads(message)
+    COMPARE_PRICE = price_data['k']['c']
+    print("compare price is ", COMPARE_PRICE)
+    if FIRST_PRICE_SET:
+        # Compare current price with ask & bid orders price
+        # if price has past any of the limits set
+        # Cancel existing orders
+        # place new orders
+        # update values of current, bid and ask price
+        print("inside first price set loop")
+    else:
+        CURRENT_PRICE = price_data['k']['c']
+        BID_ORDER_PRICE = CURRENT_PRICE - 100
+        ASK_ORDER_PRICE = CURRENT_PRICE + 100
+        print("current price is ", CURRENT_PRICE) # Not printing, only first print statment gets executed due to frequency of messages
+        print("bid order price is ", BID_ORDER_PRICE)
+        print("ask order price is ", ASK_ORDER_PRICE)
+        FIRST_PRICE_SET = True    
+    
 
-ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
-ws.run_forever()
+try:
+    ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+    ws.run_forever()
+except KeyboardInterrupt:
+    ws.close()
+    print("Connection Ended")
